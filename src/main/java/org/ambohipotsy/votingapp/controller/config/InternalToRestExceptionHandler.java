ea0415071415375daa94385edd85fa05bf9391a3
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-29 22:18:53 +0300

    chore: handle server runtime error message

--- src/main/java/org/ambohipotsy/votingapp/controller/config/InternalToRestExceptionHandler.java
+++ src/main/java/org/ambohipotsy/votingapp/controller/config/InternalToRestExceptionHandler.java
@@ -2,15 +2,19 @@ package org.ambohipotsy.votingapp.controller.config;
 
 import org.ambohipotsy.votingapp.model.exceptions.ApiException;
 import org.ambohipotsy.votingapp.model.utilities.RestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class InternalToRestExceptionHandler {
-    @ExceptionHandler(value = {ApiException.class})
-    ResponseEntity<RestException> handleApiException(ApiException apiException) {
-        return new ResponseEntity<>(toRest(apiException), apiException.getStatus());
+    @ExceptionHandler(value = {ApiException.class, RuntimeException.class})
+    ResponseEntity<RestException> handleApiException(RuntimeException exception) {
+        if (exception instanceof ApiException apiException) {
+            return new ResponseEntity<>(toRest(apiException), apiException.getStatus());
+        }
+        return new ResponseEntity<>(toRest(exception), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private RestException toRest(ApiException apiException) {
@@ -18,6 +22,14 @@ public class InternalToRestExceptionHandler {
                 .code(apiException.getStatus().value())
                 .message(apiException.getMessage())
                 .status(apiException.getStatus().toString())
+                .build();
+    }
+
+    private RestException toRest(RuntimeException runtimeException) {
+        return RestException.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(runtimeException.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.toString())
                 .build();
     }
 }
